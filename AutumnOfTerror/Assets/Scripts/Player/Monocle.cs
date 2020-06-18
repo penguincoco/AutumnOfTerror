cfb2e819fedc47550d284f468cd7712ee361a9db
Sammy Chuang <42942569+penguincoco@users.noreply.github.com>
2020-06-18 18:03:15 -0400

    Added window and smokestack models, added simple Monocle implementation.

---
+++ AutumnOfTerror/Assets/Scripts/Player/Monocle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monocle : MonoBehaviour
+{
+    bool used = false;
+    public float activeTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !used)
+        {
+            StartCoroutine(MonocleActive(activeTime));
+        }
+    }
+
+    IEnumerator MonocleActive(float activeTime)
+    {
+        //get all GameObjects in the scene that are tagged as "Clue"
+        GameObject[] clueObjs = GameObject.FindGameObjectsWithTag("Clue");
+
+        foreach (GameObject clueObj in clueObjs)
+        {
+            clueObj.gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        yield return new WaitForSeconds(activeTime);
+
+        foreach (GameObject clueObj in clueObjs)
+        {
+            clueObj.gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        used = true;
+    }
+}
