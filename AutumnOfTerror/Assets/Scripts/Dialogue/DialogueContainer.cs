21f0a40c1c722d6f467d7a82cb5c8c2732b3b033
Ronojoy Mitra <ronojoymitra@gmail.com>
2020-07-08 12:17:13 -0400

    DialogueContainerstuff

---
+++ AutumnOfTerror/Assets/Scripts/Dialogue/DialogueContainer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueContainer : MonoBehaviour
+{
+    private ObjectTimeAndDialogueList objectTimeAndDialogueList;
+    Dictionary<string, ObjectTimeAndDialogue> dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(JsonUtility.ToJson(objectTimeAndDialogueList).ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class ObjectTimeAndDialogue
+{
+    public string objectName;
+    public string time;
+    public string dialogue;
+}
+
+[System.Serializable]
+public class ObjectTimeAndDialogueList
+{
+    public List<ObjectTimeAndDialogue> objectTimeAndDialogueList;
+}
