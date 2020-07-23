d8f269997e02da4b5b9bd8587a29e0e17cf7bb07
Sammy Chuang <42942569+penguincoco@users.noreply.github.com>
2020-07-23 16:00:19 -0400

    Renamed a few models. Finished pub and it's ready to be lit. Added notebook graphic and updated the NPC data page. Began working on filling in main street. Renamed a few assets to be shorter names.

--- AutumnOfTerror/Assets/Scripts/Notebook/Notebook.cs
+++ AutumnOfTerror/Assets/Scripts/Notebook/Notebook.cs
@@ -7,7 +7,10 @@ using TMPro;
 public class Notebook : MonoBehaviour
 {
     public List<NPCObject> NPCs;
-    public TextMeshProUGUI NPCData;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI sex;
+    public TextMeshProUGUI occupation;
+    public TextMeshProUGUI address;
     public int counter = 0;
 
     void Update()
@@ -25,8 +28,20 @@ public class Notebook : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            NPCData.text = NPCs[counter].name;
+            name.text = NPCs[counter].name;
+            sex.text = NPCs[counter].sex;
+            occupation.text = NPCs[counter].occupation;
+            address.text = NPCs[counter].address;
+
             counter++;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        foreach (NPCObject npc in NPCs)
+        {
+            npc.suspectEvidence.Clear();
         }
     }
 }
